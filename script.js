2efd575ba75d384d8211277ee0c617d3fbdc1ab4
Glitch (glitch-hello-website) <none>
2022-03-08 02:29:59 +0000

    🍱🌏 Checkpoint ./script.js:42912355/1395 ./.glitch-assets:42912355/573 ./index.html:42912355/236 ./style.css:42912355/66

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const clueHoldTime = 100;
+
 var pattern = [2,2,4,3,2,1,2,4];
 var progress = 0;
 var gamePlaying = false;
@@ -16,3 +18,59 @@ function stopGame(){
   document.getElementById("startBtn").classList.remove("hidden");
   document.getElementById("stopBtn").classList.add("hidden");
 }
+
+// Sound Synthesis Functions, numbers represent sound pitch
+const freqMap = {
+  1: 261.6,
+  2: 329.6,
+  3: 392,
+  4: 466.2
+}
+function playTone(btn,len){ 
+  o.frequency.value = freqMap[btn]
+  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+  context.resume()
+  tonePlaying = true
+  setTimeout(function(){
+    stopTone()
+  },len)
+}
+function startTone(btn){
+  if(!tonePlaying){
+    context.resume()
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+    context.resume()
+    tonePlaying = true
+  }
+}
+function stopTone(){
+  g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+  tonePlaying = false
+}
+
+function lightButton(btn){
+  document.getElementById("button"+btn).classList.add("lit");
+}
+
+function clearButton(btn){
+  document.getElementById("button"+btn).classList.remove("lit");
+}
+
+function playSingleClue(btn){
+  if(gamePlaying){
+    lightButton(btn);
+    playTone(btn, clueHoldTime);
+    setTimeout(clearButton, clueHoldTime,btn);
+  }
+}
+// Page Initialization
+// Init Sound Synthesizer
+var AudioContext = window.AudioContext || window.webkitAudioContext 
+var context = new AudioContext()
+var o = context.createOscillator()
+var g = context.createGain()
+g.connect(context.destination)
+g.gain.setValueAtTime(0,context.currentTime)
+o.connect(g)
+o.start(0)
